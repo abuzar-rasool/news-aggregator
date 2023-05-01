5ca9cdf8b7da1f16018062e470e3cf9b8a99e867
Abuzar Rasool <availabuzar@gmail.com>
2023-05-01 14:05:46 +0500

    [DOCUMENTATION] Fixed swagger ui and added docs

--- core/api/views.py
+++ core/api/views.py
@@ -1,18 +1,34 @@
+"""
+This module contains two API views for News and Favourites management. Both views are
+protected by authentication.
+
+Dependencies:
+- serializers: NewsItemSerializer, FavouriteSerializer
+- rest_framework classes: Response, extend_schema, OpenApiParameter, extend_schema_view,
+  api_view, permission_classes, IsAuthenticated, generics
+- custom_base_exception: CustomBaseException
+- services: news_aggregator_service, favourite_service
+"""
 
 from .serializers import NewsItemSerializer, FavouriteSerializer
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema, OpenApiParameter, extend_schema_view
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import  IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import generics
 from core.custom_base_exception import CustomBaseException
-# services
+
 from core.news_aggregator_service import news_aggregator_service
 from core.favourite_service.favourite_service import favourite_service
 
 
 class NewsAPIView(generics.GenericAPIView):
+    """
+    NewsAPIView is a generic API view that handles fetching news data based on a given query.
+    
+    Permissions:
+    - IsAuthenticated: Ensures that only authenticated users can access this view.
+    """
     permission_classes = [IsAuthenticated]
 
     @extend_schema(
@@ -22,6 +38,12 @@ class NewsAPIView(generics.GenericAPIView):
         ],
     )
     def get(self, request, *args, **kwargs):
+        """
+        Fetches news data based on the given query and returns the serialized response.
+        The data is fetched from news_aggregator_service which is responsible for fetching
+        and caching news data from external sources.
+
+        """
         try:
             query = request.query_params.get('query', None)
             news = news_aggregator_service.fetch_combined_news_data(query)
@@ -49,9 +71,19 @@ class NewsAPIView(generics.GenericAPIView):
     )
 )
 class FavouriteAPIView(generics.ListAPIView):
+    """
+    FavouriteAPIView is a generic List API view that handles managing user's favourite news.
+    
+    Permissions:
+    - IsAuthenticated: Ensures that only authenticated users can access this view.
+    """
     permission_classes = [IsAuthenticated]
     
     def post(self, request, *args, **kwargs):
+        """
+        Toggles a news item as favourite for the given user and returns the updated favourites list.
+        
+        """
         try:
             favourites = favourite_service.toggle_favourite(request.query_params.get('user', None), request.query_params.get('id', None))
             serializer = FavouriteSerializer(favourites, many=True)
@@ -62,8 +94,11 @@ class FavouriteAPIView(generics.ListAPIView):
             return Response({'message': 'Something went wrong'}, status=500)
     
 
-    
     def get(self, request, *args, **kwargs):
+        """
+        Fetches the favourite news list for the given user and returns the serialized response.
+        
+        """
         try:
             favourites = favourite_service.get_favourites(request.query_params.get('user', None))
             serializer = FavouriteSerializer(favourites, many=True)
@@ -72,3 +107,5 @@ class FavouriteAPIView(generics.ListAPIView):
             return Response({'message': e.message}, status=e.status_code)
         except Exception as e:
             return Response({'message': 'Something went wrong'}, status=500)
+    
+
